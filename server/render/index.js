97b7e9b409b70f531354d0dc0b0f8abda0746e78
Jon Sherrard <jon@andthats.it>
2018-02-11 03:30:33 +0000

    bit of tidying

--- server/render/index.js
+++ server/render/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
+
 export default function(Component) {
-  console.log(Component)
   return renderToStaticMarkup(<Component />)
 }
