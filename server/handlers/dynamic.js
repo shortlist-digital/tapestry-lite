009e9c54243934f5c966189bae487147d0d64e66
Jon Sherrard <jon@andthats.it>
2018-02-11 04:03:19 +0000

    a bit of tidying

--- server/handlers/dynamic.js
+++ server/handlers/dynamic.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { matchRoutes } from 'react-router-config'
-import { renderStatic } from 'glamor/server'
+import { renderStaticOptimized } from 'glamor/server'
 import { renderToStaticMarkup, renderToString } from 'react-dom/server'
 import Helmet from 'react-helmet'
 
@@ -15,25 +15,24 @@ export default ({ server }) => {
     path: '/{path*}',
     handler: async function (request, h) {
       const branch = matchRoutes(appConfig.routes, request.url.pathname)
+      // Make this more robust
       const route = branch[0].route
+      // Steal the endpoint data resolver and normalisation bits
+      // from normal tapestry
       const endpoint = route.endpoint(branch[0].match.params)
       const url = `${appConfig.siteUrl}/wp-json/wp/v2/${endpoint}`
-      const data = await fetcher(url)
-      const responseBody = await data.json()
-      const App = () =>
-        <route.component {...responseBody} />
-      const { html, css, ids } = renderStatic(() =>
-        renderToString(<route.component {...responseBody} />))
-
+      const responseBody= await fetcher(url)
+      const data = await responseBody.json()
+      const { html, css, ids } = renderStaticOptimized(() =>
+        renderToString(<route.component {...data} />))
       const renderData = {
         html,
         css,
         ids,
         head: Helmet.rewind(),
-        bootstrapData: responseBody
+        bootstrapData: data
       }
-
-      const responseString = `<!doctype html>${renderToStaticMarkup(<Document {...renderData} />)}`
+      const responseString = renderToStaticMarkup(<Document {...renderData} />)
       return h
         .response(responseString)
         .type('text/html')
