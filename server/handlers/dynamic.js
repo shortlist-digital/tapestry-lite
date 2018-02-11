e89c6a25630f2eb9f4ce96ad790fd4acc31fe637
Jon Sherrard <jon@andthats.it>
2018-02-11 04:10:15 +0000

    some comments

--- server/handlers/dynamic.js
+++ server/handlers/dynamic.js
@@ -4,9 +4,11 @@ import { renderStaticOptimized } from 'glamor/server'
 import { renderToStaticMarkup, renderToString } from 'react-dom/server'
 import Helmet from 'react-helmet'
 
-import renderHtml from '../render'
+// Same default document
 import Document from '../render/default-document'
+// Using an old dummy app I have for now
 import appConfig from '../../test-app/tapestry.config'
+// Tuned fetched from normal tapestry
 import fetcher from '../../shared/fetcher'
 
 export default ({ server }) => {
@@ -14,6 +16,8 @@ export default ({ server }) => {
     method: 'GET',
     path: '/{path*}',
     handler: async function (request, h) {
+      // Don't even import react-router any more, but backwards compatible
+      // With the exception of optional params: (:thing) becomes :thing?
       const branch = matchRoutes(appConfig.routes, request.url.pathname)
       // Make this more robust
       const route = branch[0].route
@@ -21,10 +25,13 @@ export default ({ server }) => {
       // from normal tapestry
       const endpoint = route.endpoint(branch[0].match.params)
       const url = `${appConfig.siteUrl}/wp-json/wp/v2/${endpoint}`
+      // Async/Await dereams
       const responseBody= await fetcher(url)
       const data = await responseBody.json()
+      // Glamor works as before
       const { html, css, ids } = renderStaticOptimized(() =>
         renderToString(<route.component {...data} />))
+      // Assets to come, everything else works
       const renderData = {
         html,
         css,
@@ -33,6 +40,7 @@ export default ({ server }) => {
         bootstrapData: data
       }
       const responseString = renderToStaticMarkup(<Document {...renderData} />)
+      // Respond with new Hapi 17 api
       return h
         .response(responseString)
         .type('text/html')
@@ -40,4 +48,3 @@ export default ({ server }) => {
     }
   })
 }
-
