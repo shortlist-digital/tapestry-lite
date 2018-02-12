17758b61f3425f950bd30b67c348b38a3abbbd98
Jon Sherrard <jon@andthats.it>
2018-02-12 23:53:04 +0000

    Add inert plugin

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 import server from './server'
+import Inert from 'inert'
 
 let currentApp
 const timeLabel = 'Hot Reload Restart'
 
+const serverPlugins = [
+  Inert
+]
+
 const run = async function() {
   try {
     currentApp = server
+    // Register plugins
+    await server.register(serverPlugins)
+    // Start server
     await currentApp.start()
     if (module.hot) {
       module.hot.accept('./server', async function() {
         console.time(timeLabel)
         await currentApp.stop({ timeout: 0 })
         currentApp = server
+        // Re-register plugins
+        await server.register(serverPlugins)
         await currentApp.start()
         console.timeEnd(timeLabel)
       })
