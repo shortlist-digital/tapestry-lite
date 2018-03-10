ee1a1e594b004443d548ff3dc21287bfb71d1a92
Jon Sherrard <jon@andthats.it>
2018-03-10 23:07:21 +0000

    weird react-inspector bug

--- src/server/render/missing-view/index.js
+++ src/server/render/missing-view/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ObjectInspector from 'react-object-inspector'
+import { ObjectInspector } from 'react-inspector'
 import DefaultError from '../default-error'
 
 const MissingView = response => (
   <DefaultError {...response}>
     <h1>Missing component</h1>
     {response && (
-      <ObjectInspector data={response} initialExpandedPaths={['root']} />
+      <ObjectInspector data={response} expandLevel={1} />
     )}
   </DefaultError>
 )
