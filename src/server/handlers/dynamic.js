790b098f8e9e3cf577402d244480d000683f5bf4
Jon Sherrard <jon@andthats.it>
2018-03-11 02:07:05 +0000

    some noodling to get hot reloading back - no udpates to parent projects

--- src/server/handlers/dynamic.js
+++ src/server/handlers/dynamic.js
@@ -42,6 +42,7 @@ export default ({ server, config }) => {
         let errorData = false
         if (endpoint) {
           const url = `${baseUrlResolver(config)}/${endpoint}`
+          console.log({url})
           // Async/Await dereams
           allowEmptyResponse = idx(route, _ => _.options.allowEmptyResponse)
 
