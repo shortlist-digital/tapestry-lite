77105847ee6d94a374ba5f45f4bd15795d0aba9d
Jon Sherrard <jon@andthats.it>
2018-03-12 19:05:09 +0000

    finish up data fetching backwards compatibility

--- src/server/data-fetching/normalize-api-response.js
+++ src/server/data-fetching/normalize-api-response.js
@@ -1,10 +1,16 @@
 import idx from 'idx'
 import HTTPStatus from 'http-status'
 import isEmpty from 'lodash.isempty'
+import isPlainObject from 'lodash.isplainobject'
+import isFunction from 'lodash.isfunction'
 
 export default (response, route) => {
   // WP returns single objects or arrays
   const arrayResp = Array.isArray(response)
+  // We can configure a route response to be an object "on purpose"
+  // We can't check if the response is supposed to be an object inside the function,
+  // so we dangerously assume it's on purpose if it's in a function
+  const objectOnPurpose = isPlainObject(route.endpoint) || isFunction(route.endpoint)
   // 1: is it a falsey value
   // 2: does it contain a status code? then it'll be an error response from WP
   // 3: is it an array, that is empty, and options.allowEmptyResponse is falsey
@@ -32,5 +38,5 @@ export default (response, route) => {
   }
   // to avoid React mangling the array to {'0':{},'1':{}}
   // wrap in an object
-  return arrayResp ? { data: response } : response
+  return (objectOnPurpose || arrayResp) ? { data: response } : response
 }
--- test/server/routing.test.js
+++ test/server/routing.test.js
@@ -113,3 +113,136 @@ describe('Handling custom endpoint routes', () => {
   })
 })
 
+describe('Handling custom endpoint routes', () => {
+  let server = null
+  let uri = null
+  let config = {
+    routes: [
+      {
+        path: '/string-endpoint',
+        endpoint: 'pages',
+        component: () => <p>Basic endpoint</p>
+      },
+      {
+        path: '/array-endpoint',
+        endpoint: ['pages', 'posts'],
+        component: () => <p>Custom endpoint</p>
+      },
+      {
+        path: '/object-endpoint',
+        endpoint: {
+          pages: 'pages',
+          posts: 'posts'
+        },
+        component: () => <p>Custom endpoint</p>
+      },
+      {
+        path: '/dynamic-string-endpoint/:custom',
+        endpoint: params => `pages?slug=${params.custom}`,
+        component: () => <p>Custom endpoint</p>
+      },
+      {
+        path: '/dynamic-array-endpoint/:custom',
+        endpoint: params => [
+          `pages?slug=${params.custom}`,
+          `posts?slug=${params.custom}`
+        ],
+        component: () => <p>Custom endpoint</p>
+      },
+      {
+        path: '/dynamic-object-endpoint/:custom',
+        endpoint: params => ({
+          page: `pages?slug=${params.custom}`,
+          post: `posts?slug=${params.custom}`
+        }),
+        component: () => <p>Custom endpoint</p>
+      }
+    ],
+    siteUrl: 'http://dummy.api'
+  }
+
+  before(async () => {
+    // mock api response
+    nock('http://dummy.api')
+      .get('/wp-json/wp/v2/pages')
+      .times(5)
+      .reply(200, dataPages.data)
+      .get('/wp-json/wp/v2/posts')
+      .times(5)
+      .reply(200, dataPosts.data)
+      .get('/wp-json/wp/v2/posts?slug=test')
+      .times(5)
+      .reply(200, dataPost)
+      .get('/wp-json/wp/v2/pages?slug=test')
+      .times(5)
+      .reply(200, dataPage)
+    // boot tapestry server
+    server = new Server({config})
+    await server.start()
+    uri = server.info.uri
+  })
+
+  after(async () => server.stop())
+
+  it('Route matched, string endpoint works', done => {
+    request.get(`${uri}/string-endpoint`, (err, res, body) => {
+      expect(body).to.contain(prepareJson(dataPages.data))
+      done()
+    })
+  })
+
+  it('Route matched, array endpoint works', done => {
+    request.get(`${uri}/array-endpoint`, (err, res, body) => {
+      const expectedJson = {
+        data: [dataPages.data, dataPosts.data]
+      }
+      expect(body).to.contain(prepareJson(expectedJson))
+      done()
+    })
+  })
+
+  it('Route matched, object endpoint works', done => {
+    request.get(`${uri}/object-endpoint`, (err, res, body) => {
+      const expectedJson = {
+        data: {
+          pages: dataPages.data,
+          posts: dataPosts.data
+        }
+      }
+      expect(body).to.contain(prepareJson(expectedJson))
+      done()
+    })
+  })
+
+  it('Route matched, dynamic string endpoint works', done => {
+    request.get(`${uri}/dynamic-string-endpoint/test`, (err, res, body) => {
+      expect(body).to.contain(prepareJson(dataPage))
+      done()
+    })
+  })
+
+  it('Route matched, dynamic array endpoint works', done => {
+    request.get(`${uri}/dynamic-array-endpoint/test`, (err, res, body) => {
+      const expectedJson = {
+        data: [dataPage, dataPost]
+      }
+      expect(body).to.contain(prepareJson(expectedJson))
+      done()
+    })
+  })
+
+  it('Route matched, dynamic object endpoint works', done => {
+    request.get(`${uri}/dynamic-object-endpoint/test`, (err, res, body) => {
+      const expectedJson = {
+        data: {
+          page: dataPage,
+          post: dataPost
+        }
+      }
+      expect(body).to.contain(prepareJson(expectedJson))
+      done()
+    })
+  })
+})
+
+
