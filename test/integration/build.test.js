f4bde1512bc218a06370e559d8df36660631a4fd
thomasdigby <thomasdigby1@gmail.com>
2018-03-14 17:32:58 +0000

    meh

--- test/integration/build.test.js
+++ test/integration/build.test.js
@@ -15,6 +15,8 @@ describe('tapestry build', () => {
   it('should compile files into a build directory', () => {
     shell.exec('node ../bin/build.js')
     expect(shell.test('-f', '.tapestry/server.js')).to.equal(true)
+    // hash from file contents of " body { color: red; } "
+    expect(shell.test('-f', '.tapestry/b6452d8fa43c7cb57e6b0f9bd745209a.css')).to.equal(true)
   }).timeout(5000)
 
   after(() => {
