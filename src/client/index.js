98d0860967a8fe5a1afdd16f0c941f8ed17ad574
thomasdigby <thomasdigby1@gmail.com>
2018-03-28 12:29:31 +0100

    comment

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import { hydrate } from 'react-dom'
 import config from '../config/config-proxy'
 import Root from './root'
 
-// hydrate emotion CSS 
+// Hydrate server rendered CSS with either Emotion or Glamor
 if (__CSS_PLUGIN__ === 'emotion') {
   require('emotion').hydrate(window.__data.ids)
 } else {
