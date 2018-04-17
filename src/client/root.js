e6ead289ab6d23c76a59207e59a42033a1322a57
thomasdigby <thomasdigby1@gmail.com>
2018-04-17 17:21:33 +0100

    No more lyrics

--- src/client/root.js
+++ src/client/root.js
@@ -5,17 +5,20 @@ import prepareAppRoutes from '../server/routing/prepare-app-routes'
 import matchRoutes from '../server/routing/match-routes'
 import buildErrorView from '../server/render/error-view'
 
+const log = (msg, data) =>
+  window.location.search.indexOf('debug') !== -1 && console.log(msg, data)
+
 const Root = config => {
+  log(`Application data`, window.__data)
   if (window.__data.appData.code === 404) {
     const Component = buildErrorView({ config, missing: false })
     return <Component {...window.__data.appData} />
   }
-  console.log(`Application data`, window.__data)
   const { route, match } = matchRoutes(
     prepareAppRoutes(config),
     window.location.pathname
   )
-  console.log('Matched route', route)
+  log('Matched route', route)
   return <route.component {...match} {...window.__data.appData} />
 }
 
