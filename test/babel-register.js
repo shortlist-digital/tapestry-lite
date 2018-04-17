e04564f5abe48680e1d3180b1dee00408b5dda89
thomasdigby <thomasdigby1@gmail.com>
2018-04-17 20:22:09 +0100

    Fix babel call as function

--- test/babel-register.js
+++ test/babel-register.js
@@ -6,7 +6,4 @@ process.on('unhandledRejection', (reason, p) => {
 })
 // This file is required in mocha.opts
 // The only purpose of this file is to ensure the babel transpiler is activated prior to any test code, and using the same babel options
-require('@babel/register')({
-  sourceMap: 'inline',
-  ...babelDefaultConfig
-})
+require('@babel/register')(babelDefaultConfig())
