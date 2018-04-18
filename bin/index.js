fd6072a112a36199aa430b953838d5027a8f79ab
thomasdigby <thomasdigby1@gmail.com>
2018-04-18 16:46:47 +0100

    Revert stupid process.env check

--- bin/index.js
+++ bin/index.js
@@ -10,17 +10,13 @@ const args = process.argv.slice(3)
 
 const validCommands = ['start', 'build', 'dev']
 
-const logError = msg => {
-  console.log(`\n${chalk.red('Error:')} ${msg}\n`)
-  process.exit(0)
-}
-
 if (command && !validCommands.includes(command)) {
-  logError(`Unknown command ${chalk.green(`tapestry ${command}`)}`)
-}
-
-if (!process.env.CMS_ENDPOINT) {
-  logError('Missing a CMS_ENDPOINT process.env')
+  console.log(
+    `\n${chalk.red('Error:')} Unknown command ${chalk.green(
+      `tapestry ${command}`
+    )}\n`
+  )
+  process.exit(0)
 }
 
 spawn.sync('node', [require.resolve(`./${command || 'dev'}`)].concat(args), {
