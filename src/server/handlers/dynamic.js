d632b43a60174e3cfb7e85903397911b6b5d8ce4
Jon Sherrard <jon@andthats.it>
2018-04-19 16:39:13 +0100

    Split rendering of success and error views

--- src/server/handlers/dynamic.js
+++ src/server/handlers/dynamic.js
@@ -13,6 +13,33 @@ import baseUrlResolver from '../utilities/base-url-resolver'
 import CacheManager from '../utilities/cache-manager'
 import { log } from '../utilities/logger'
 
+const renderErrorTree = async ({ route, match, componentData, h }) => {
+  log.silly('Rendering Error HTML')
+  const responseString = await renderTreeToHTML({
+    route,
+    match,
+    componentData
+  })
+  log.silly('Error Data: ', componentData)
+  return h
+    .response(responseString)
+    .type('text/html')
+    .code(componentData.code || 404)
+}
+
+const renderSuccessTree = async ({ route, match, componentData, h }) => {
+  log.silly('Rendering Success HTML', { match })
+  const responseString = await renderTreeToHTML({
+    route,
+    match,
+    componentData
+  })
+  return h
+    .response(responseString)
+    .type('text/html')
+    .code(200)
+}
+
 export default ({ server, config }) => {
   let cacheManager = new CacheManager()
   const cache = cacheManager.createCache('html')
@@ -57,50 +84,35 @@ export default ({ server, config }) => {
         status: 200,
         message: '200'
       }
-      let fetchRequestHasErrored = false
 
       // Set a flag for whether we have a missing component later on
       const routeComponentUndefined = typeof route.component === 'undefined'
       // If we have an endpoint
       if (route.endpoint) {
         // Start to try and fetch data
-        try {
-          const multidata = await fetchFromEndpointConfig({
-            endpointConfig: route.endpoint,
-            baseUrl: baseUrlResolver(config, request.url),
-            requestUrlObject: request.url,
-            params: match.params
-          })
-          // log.silly(
-          //   `HTML: Result from ${chalk.green('fetchFromEndpointConfig')}`,
-          //   multidata
-          // )
-          // If we received data without throwing - normalize it
-          componentData = normalizeApiResponse(multidata, route)
-          log.silly(`Endpoint data fetched and normalized:`, componentData)
-        } catch (e) {
-          // There has eiter been a 'natural 404', or we've thrown one
-          // due to an empty response from a WP endpoint
-          log.debug('Multidata failed', e)
-          componentData = e
-          fetchRequestHasErrored = true
-          log.error(`Endpoint data failed to fetch:`, componentData)
-        } // End of fetching 'try' block
-      } // End of 'if endpoint' block
-      // We now have componentData
-      // If there's no component, and have a 4xx or 5xx error - and
-      // an empty response is not allowed, we replace the route component
-      // with our error view component
-      if (componentData.code === 404 || routeComponentUndefined) {
+        const multidata = await fetchFromEndpointConfig({
+          endpointConfig: route.endpoint,
+          baseUrl: baseUrlResolver(config, request.url),
+          requestUrlObject: request.url,
+          params: match.params
+        })
+        componentData = normalizeApiResponse(multidata, route)
+      }
+      if (componentData.code > 299 || routeComponentUndefined) {
         log.debug(`Render Error component`, {
           componentData,
-          routeComponentUndefined,
-          fetchRequestHasErrored
+          routeComponentUndefined
         })
 
         route.component = buildErrorView({
           config,
           missing: routeComponentUndefined
+        })
+        return renderErrorTree({
+          route,
+          match,
+          componentData,
+          h
         })
       }
 
@@ -112,27 +124,20 @@ export default ({ server, config }) => {
           message: 'Not Found',
           code: 404
         }
+        return renderErrorTree({
+          route,
+          match,
+          componentData,
+          h
+        })
       }
 
-      // Render the route with componentData, the route
-      const responseString = await renderTreeToHTML({
+      return renderSuccessTree({
         route,
         match,
-        componentData
+        componentData,
+        h
       })
-
-      // Set status code
-      const code = componentData.code || 200
-      if (code == 200) {
-        log.debug(`Set HTML in cache ${chalk.green(cacheKey)}`)
-        cache.set(cacheKey, responseString)
-      }
-      log.debug('Rendering HTML from scratch')
-      // Respond with new Hapi 17 api
-      return h
-        .response(responseString)
-        .type('text/html')
-        .code(code)
     }
   })
 }
