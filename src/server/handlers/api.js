a4985f664d8a74973d1bf10f1d62e769077e4386
thomasdigby <thomasdigby1@gmail.com>
2018-05-24 09:58:00 +0100

    Handle /api/v1/ requests and proxy through to CMS

---
+++ src/server/handlers/api.js
@@ -0,0 +1,18 @@
+import baseUrlResolver from '../utilities/base-url-resolver'
+
+export default ({ server, config }) => {
+  const base = baseUrlResolver(config)
+  server.route({
+    method: 'GET',
+    path: '/api/v1/{query*}',
+    config: {
+      cache: {
+        expiresIn:
+          (parseInt(process.env.CACHE_CONTROL_MAX_AGE, 10) || 0) * 1000, // 1 Minute
+        privacy: 'public'
+      }
+    },
+    handler: ({ params, url }, h) =>
+      h.proxy({ uri: `${base}/${params.query}${url.search}` })
+  })
+}
