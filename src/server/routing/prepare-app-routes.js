3f377d1e5b6cae1d85e54601a6aa5efc070c3df3
thomasdigby <thomasdigby1@gmail.com>
2018-09-07 15:00:23 +0100

    Filter by bool in correct place

--- src/server/routing/prepare-app-routes.js
+++ src/server/routing/prepare-app-routes.js
@@ -1,10 +1,12 @@
 import errorView from '../render/error-view'
 
 export default config => {
-  let routes = config.routes.filter(Boolean)
+  let routes = config.routes
   // import and use default routes if user has not defined any routes
   if (!routes) routes = require('./default-routes').default(config.components)
-  const preparedRoutes = routes.map(route => ({ ...route, exact: true }))
+  const preparedRoutes = routes
+    .filter(Boolean)
+    .map(route => ({ ...route, exact: true }))
   // add a default "not found" route
   preparedRoutes.push({
     path: '(.*)',
