b343eced2c5747f1aec8518faa25726b2149f215
thomasdigby <thomasdigby1@gmail.com>
2019-01-21 15:10:03 +0000

    Only include default error/missing-view in DEVELOPMENT env

--- src/server/render/error-view.js
+++ src/server/render/error-view.js
@@ -1,19 +1,15 @@
-import DefaultError from './default-error'
-import MissingView from './missing-view'
-
 export default ({ config = {}, missing = false }) => {
-  // render custom error or default if custom error not declared
-  let ErrorView =
-    config.components && config.components.CustomError
-      ? config.components.CustomError
-      : DefaultError
+  let ErrorView = () => null
   // render missing component only in DEV
   if (
-    (process.env.NODE_ENV === 'test' ||
-      process.env.NODE_ENV === 'development') &&
-    missing
+    process.env.NODE_ENV === 'test' ||
+    process.env.NODE_ENV === 'development'
   ) {
-    ErrorView = MissingView
+    ErrorView = missing ? require('./missing-view') : require('./default-error')
+  }
+  // render custom error
+  if (config.components && config.components.CustomError) {
+    ErrorView = config.components.CustomError
   }
   // return one of the error views
   return ErrorView
