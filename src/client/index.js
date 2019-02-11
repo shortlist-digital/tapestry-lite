2d26d36421a7668bd189da5130c28b1bb43f5ade
thomasdigby <thomasdigby1@gmail.com>
2019-02-11 12:48:10 +0000

    Remove tapestry preview querystring in browser

--- src/client/index.js
+++ src/client/index.js
@@ -8,6 +8,23 @@ import { loadComponents } from 'loadable-components'
 import config from '../config/config-proxy'
 import Root from './root'
 
+// If page is a WP preview, remove any tapestry related querystring data
+// This runs on the client only so it won't affect loading preview data
+// Added check for the history API for compatibility with other browsers
+if (
+  window.location.search.indexOf('tapestry_hash') > -1 &&
+  window.history &&
+  window.history.replaceState
+) {
+  window.history.replaceState(
+    {},
+    document.title,
+    `${window.location.protocol}//${window.location.host}${
+      window.location.pathname
+    }`
+  )
+}
+
 // Hydrate server rendered CSS with either Emotion or Glamor
 if (CSS_PLUGIN === 'emotion')
   require('emotion').hydrate(window.__TAPESTRY_DATA__.ids)
