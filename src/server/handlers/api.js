2228c1d1462f6e5904df681ca66269f40dd7b589
thomasdigby <thomasdigby1@gmail.com>
2019-05-08 10:38:58 +0100

    Integrate new baseUrl in API and Dynamic

--- src/server/handlers/api.js
+++ src/server/handlers/api.js
@@ -1,7 +1,8 @@
 import baseUrlResolver from '../utilities/base-url-resolver'
+import prepareAppRoutes from '../routing/prepare-app-routes'
+import matchRoutes from '../routing/match-routes'
 
 export default ({ server, config }) => {
-  const base = baseUrlResolver(config)
   server.route({
     method: 'GET',
     path: '/api/v1/{query*}',
@@ -12,10 +13,14 @@ export default ({ server, config }) => {
         privacy: 'public'
       }
     },
-    handler: ({ params, url }, h) =>
-      h.proxy({
+    handler: ({ params, url }, h) => {
+      // get matching route and match data
+      const { route } = matchRoutes(prepareAppRoutes(config), url.pathname)
+      const base = baseUrlResolver(config, params.query, route)
+      return h.proxy({
         uri: `${base}/${params.query}${url.search || ''}`,
         passThrough: true
       })
+    }
   })
 }
