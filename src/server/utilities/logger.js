5710fa05f3111d005b24e171d41efc2758de004c
Romulo De Lazzari <romulodelazzari@gmail.com>
2019-05-16 13:09:02 +0100

    add console log for NODE_ENV=development

--- src/server/utilities/logger.js
+++ src/server/utilities/logger.js
@@ -8,14 +8,25 @@ const logFormat = printf(({ level, message, timestamp }) => {
 const log = createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(timestamp(), logFormat),
-  transports: [
-    new transports.File({
-      filename: process.env.LOG_PATH + '/error.log',
-      level: 'error'
-    }),
-    new transports.File({ filename: process.env.LOG_PATH + '/access.log' })
-  ]
+  transports:
+    process.env.PORT !== process.env.TAPESTRY_PORT
+    ? [
+      new transports.File({ filename: process.env.LOG_PATH + '/error.log', level: 'error' }),
+      new transports.File({ filename: process.env.LOG_PATH + '/access.log' })
+    ]
+    : [
+      new transports.File({ filename: '/dev/stderr', level: 'error' }),
+      new transports.File({ filename: '/dev/stdout', level: 'error' }),
+    ]
 })
+
+if (process.env.NODE_ENV === 'development') {
+  log.add(
+    new transports.Console({
+      format: format.simple()
+    })
+  )
+}
 
 // instance of Winston logger for debug/error/silly logs
 module.exports.log = log
