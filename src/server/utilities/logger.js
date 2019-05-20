32b3201a08e62107e9d15d823d8deeaad1d85595
Romulo De Lazzari <romulodelazzari@gmail.com>
2019-05-20 11:49:50 +0100

    local logger to console

--- src/server/utilities/logger.js
+++ src/server/utilities/logger.js
@@ -9,24 +9,15 @@ const log = createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(timestamp(), logFormat),
   transports:
-    process.env.PORT !== process.env.TAPESTRY_PORT
+    process.env.NODE_ENV !== 'development'
     ? [
       new transports.File({ filename: process.env.LOG_PATH + '/error.log', level: 'error' }),
       new transports.File({ filename: process.env.LOG_PATH + '/access.log' })
     ]
     : [
-      new transports.File({ filename: '/dev/stderr', level: 'error' }),
-      new transports.File({ filename: '/dev/stdout', level: 'error' }),
+      new transports.Console({ format: format.simple() })
     ]
 })
-
-if (process.env.NODE_ENV === 'development') {
-  log.add(
-    new transports.Console({
-      format: format.simple()
-    })
-  )
-}
 
 // instance of Winston logger for debug/error/silly logs
 module.exports.log = log
